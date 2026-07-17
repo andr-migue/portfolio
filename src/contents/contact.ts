b37dec2da91a4e34b11bea24267326271c918c73
andr-migue <miguelzamora210405@gmail.com>
2026-07-17 06:05:27 -0400

    fix location coords

--- src/contents/contact.ts
+++ src/contents/contact.ts
@@ -2,7 +2,7 @@ export const contact = {
     toEmail: 'miguelzamora210405@gmail.com',
     location: {
         label: 'Micro X, Alamar, Habana del Este',
-        coords: [23.1616, -82.2810] as [number, number],
+        coords: [23.172247013747032, -82.25355615095722] as [number, number],
         zoom: 15,
     },
 }
