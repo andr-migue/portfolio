f5b8b054849f7b15447780199143e256a6acaefc
andr-migue <miguelzamora210405@gmail.com>
2026-07-17 06:13:45 -0400

    update zoom en location

--- src/contents/contact.ts
+++ src/contents/contact.ts
@@ -3,6 +3,6 @@ export const contact = {
     location: {
         label: 'Micro X, Alamar, Habana del Este',
         coords: [23.172247013747032, -82.25355615095722] as [number, number],
-        zoom: 15,
+        zoom: 12,
     },
 }
